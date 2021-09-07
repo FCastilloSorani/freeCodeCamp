57d50e94707b0a88415e29b8dbcee6d748994315
FCastilloSorani <fercastillo.s.1997@gmail.com>
2021-09-06 21:27:31 -0300

    app get / and express.static middleware

--- backend-APIs/myApp.js
+++ backend-APIs/myApp.js
@@ -2,13 +2,25 @@ var express = require('express');
 var app = express();
 
 // Cuando alguien accede a /, se envía el objeto respuesta "Hello Express"
-app.get(
+/* app.get(
     '/',
     function(req,res){
         res.send("Hello Express");
     }
+); */
+
+// Enviar archivo cuando alguien accede a /
+let absolutePath = __dirname + '/views/index.html';
+
+app.get(
+    '/',
+    (req,res) => {
+        res.sendFile(absolutePath);
+    }
 );
 
+// Static assets. Middleware
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
